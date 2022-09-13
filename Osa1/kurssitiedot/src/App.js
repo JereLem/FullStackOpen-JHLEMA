03b8640b0113badf16c93f8fe05182516dccfb82
JereLem <56977360+JereLem@users.noreply.github.com>
2022-09-13 17:49:57 +0300

    step4

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -21,39 +21,39 @@ const Part = (props) => {
 const Content = (props) => {
   return(
   <div>
-    <Part part={props.part} />
+    {props.parts.map(part => <Part part={part}/>)}
   </div>
   )
 }
 
 const Total = (props) => {
   return(
-  <p>Number of exercises {props.exercises}</p>
+  <p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1}/>
-      <Content part={part2}/>
-      <Content part={part3}/>
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
