23845658928be06731ba2458762abb854bf2ab97
JereLem <56977360+JereLem@users.noreply.github.com>
2022-09-13 16:54:12 +0300

    step2

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -1,7 +1,19 @@
 const Header = (props) => {
   return(
-  <div>
-    <h1>{props.course}</h1>
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
   </div>
   )
 }
@@ -9,24 +21,16 @@ const Header = (props) => {
 const Content = (props) => {
   return(
   <div>
-  <p>
-    {props.part1} {props.exercises1}
-  </p>
-  <p>
-    {props.part2} {props.exercises2}
-  </p>
-  <p>
-    {props.part3} {props.exercises3}
-  </p>
-</div>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={props.exercises3}/>
+  </div>
   )
 }
 
 const Total = (props) => {
   return(
-  <div>
   <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-</div>
   )
 }
 
@@ -41,8 +45,14 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises1}part3={part3} exercises3={exercises1}/>
+      <Header course={course} />
+      <Content 
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
