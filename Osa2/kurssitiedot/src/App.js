7ba5cf6c2ed0c770d6ef346f184f8c8383276b66
JereLem <56977360+JereLem@users.noreply.github.com>
2022-10-05 22:48:01 +0300

    step 9

--- Osa2/kurssitiedot/src/App.js
+++ Osa2/kurssitiedot/src/App.js
@@ -1,5 +1,5 @@
 const Header = ({ text }) => {
-  return <h1>{text}</h1>;
+  return <h2>{text}</h2>;
 };
 
 const Part = ({ partinfo }) => {
@@ -21,6 +21,7 @@ const Total = (parts) => {
 const Course = ({ course }) => {
   return (
     <>
+      <h1>Web development curriculum</h1>
       <Header text={course.name} />
       <Content parts={course.parts} />
       <h4> Total of {Total(course.parts)} exercises </h4>
@@ -29,33 +30,52 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return (
-    <div>
-      <Course course={course} />
-    </div>
-  )
-}
+  return courses.map((course) => <Course course={course} key ={course.id} />);
+};
 
 export default App
